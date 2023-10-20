29f88825297ff9876ae5dda0b851e324115df68a
innerviewer <innerviewerr@gmail.com>
2023-10-21 01:36:40 +0200

    (Debug) Fixed debug render pass.

--- Engine/Core/libs/Graphics/inc/Graphics/Pass/DebugPass.h
+++ Engine/Core/libs/Graphics/inc/Graphics/Pass/DebugPass.h
@@ -5,12 +5,12 @@
 #ifndef SRENGINE_DEBUGPASS_H
 #define SRENGINE_DEBUGPASS_H
 
-#include <Graphics/Pass/IMeshClusterPass.h>
+#include <Graphics/Pass/IMesh3DClusterPass.h>
 
 namespace SR_GRAPH_NS {
-    class DebugPass : public IMeshClusterPass {
+    class DebugPass : public IMesh3DClusterPass {
         SR_REGISTER_LOGICAL_NODE(DebugPass, Debug Pass, { "Passes" })
-        using Super = IMeshClusterPass;
+        using Super = IMesh3DClusterPass;
     public:
         SR_NODISCARD MeshClusterTypeFlag GetClusterType() const noexcept override;
 
