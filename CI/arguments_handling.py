81cdbb002cd845c7fc96692989908099e1a1836c
innerviewer <innerviewerr@gmail.com>
2023-10-22 05:09:08 +0200

    (CI) Implemented a script to handle arguments.

--- CI/arguments_handling.py
+++ CI/arguments_handling.py
@@ -1,4 +1,20 @@
 import sys 
 
-for i in range(len(sys.argv)):
-    print(i, " : ", str(sys.argv[i]))
+ciValues = ["#SR_CI_NONE", "#SR_CI_RELEASE", "#SR_CI_DEBUG", "#SR_CI_ALL"]
+ciArguments = []
+            
+def main():
+    for value in ciValues:
+        for arg in sys.argv:
+            if arg ==  value:
+                ciArguments.append(value)
+            
+    for argument in ciArguments:
+        print(f"{argument}=true")
+        ciValues.remove(argument)
+        
+    for value in ciValues:
+        print(f"{argument}=false")
+    
+if __name__ == "__main__":
+    main()
