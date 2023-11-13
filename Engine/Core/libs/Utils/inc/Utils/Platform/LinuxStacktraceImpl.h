4fca16ae916b455542793e09f111e194db89e304
innerviewer <innerviewerr@gmail.com>
2023-11-13 13:32:50 +0100

    (Build) Fixed Windows and Linux builds.
    #SR_CLOSE(64, 62, 56)

--- Engine/Core/libs/Utils/inc/Utils/Platform/LinuxStacktraceImpl.h
+++ Engine/Core/libs/Utils/inc/Utils/Platform/LinuxStacktraceImpl.h
@@ -1,6 +1,11 @@
+#ifndef SRENGINE_LINUXSTACKTRACEIMPL_H
+#define SRENGINE_LINUXSTACKTRACEIMPL_H
+
 #include <Utils/macros.h>
 
 namespace SR_UTILS_NS {
     extern char* GetStacktraceImpl();
     extern void StacktraceInitImpl();
 }
+
+#endif // SRENGINE_LINUXSTACKTRACEIMPL_H
