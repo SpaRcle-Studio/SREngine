887444c7db9124b8e1ee867b3d86045587fd2eae
innerviewer <innerviewerr@gmail.com>
2024-04-07 17:02:17 +0200

    (CXX) Changed paths to source files.

--- Engine/Core/libs/Scripting/Scripting.cxx
+++ Engine/Core/libs/Scripting/Scripting.cxx
@@ -4,12 +4,12 @@
 
 #include <Utils/macros.h>
 
-#include "../Scripting/src/Scripting/Base/Compiler.cpp"
-#include "../Scripting/src/Scripting/Base/Script.cpp"
-#include "../Scripting/src/Scripting/Base/Behaviour.cpp"
+#include "src/Scripting/Base/Compiler.cpp"
+#include "src/Scripting/Base/Script.cpp"
+#include "src/Scripting/Base/Behaviour.cpp"
 
-#include "../Scripting/src/Scripting/Impl/EvoScriptImpl.cpp"
-#include "../Scripting/src/Scripting/Impl/EvoCompiler.cpp"
-#include "../Scripting/src/Scripting/Impl/EvoBehaviour.cpp"
-#include "../Scripting/src/Scripting/Impl/EvoScriptManager.cpp"
-#include "../Scripting/src/Scripting/Impl/EvoScriptResourceReloader.cpp"
+#include "src/Scripting/Impl/EvoScriptImpl.cpp"
+#include "src/Scripting/Impl/EvoCompiler.cpp"
+#include "src/Scripting/Impl/EvoBehaviour.cpp"
+#include "src/Scripting/Impl/EvoScriptManager.cpp"
+#include "src/Scripting/Impl/EvoScriptResourceReloader.cpp"
