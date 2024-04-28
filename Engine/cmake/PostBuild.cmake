4ed0feed53b6b3a43444f3eba2719f91e4fc682c
innerviewer <innerviewerr@gmail.com>
2024-04-28 21:50:36 +0200

    (CMake) Disabled PostBuild.cmake copying.

--- Engine/cmake/PostBuild.cmake
+++ Engine/cmake/PostBuild.cmake
@@ -57,44 +57,45 @@ macro(SRCopyFileToApp dir file)
     SRCopyFileToAppInternal(${dir} lib${file})
 endmacro()
 
-SRCopyFileToApp("Engine/Core/libs/Graphics/EvoVulkan/Core" "EvoVulkan.dll")
-SRCopyFileToApp("Engine/Core/libs/Graphics/EvoVulkan/Core" "EvoVulkand.dll")
-
-SRCopyFileToApp("Engine/Core/libs/Utils/libs/assimp/bin" "assimp-vc142-mtd.dll")
-SRCopyFileToApp("Engine/Core/libs/Utils/libs/assimp/bin" "assimp-vc142-mtdd.dll")
-
-SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc143.md/debug" "PhysXd.dll")
-SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc143.md/debug" "PhysXCookingd.dll")
-SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc143.md/debug" "PhysXCommond.dll")
-SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc143.md/debug" "PhysXFoundationd.dll")
-
-SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc142.md/debug" "PhysXd.dll")
-SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc142.md/debug" "PhysXCookingd.dll")
-SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc142.md/debug" "PhysXCommond.dll")
-SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc142.md/debug" "PhysXFoundationd.dll")
-
-SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc143.md/release" "PhysX.dll")
-SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc143.md/release" "PhysXCooking.dll")
-SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc143.md/release" "PhysXCommon.dll")
-SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc143.md/release" "PhysXFoundation.dll")
-
-SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc142.md/release" "PhysX.dll")
-SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc142.md/release" "PhysXCooking.dll")
-SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc142.md/release" "PhysXCommon.dll")
-SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc142.md/release" "PhysXFoundation.dll")
-
-SRCopyFileToApp("Engine/Core/libs/Utils" "Utils.dll")
-SRCopyFileToApp("Engine/Core/libs/Utils" "Utilsd.dll")
-
-SRCopyFileToApp("Engine/Core/libs/Audio/libs/OpenAL" "OpenAL32.dll")
-SRCopyFileToApp("Engine/Core/libs/Audio/libs/OpenAL" "OpenAL64.dll")
-SRCopyFileToApp("Engine/Core/libs/Audio/libs/OpenAL" "OpenAL32d.dll")
-SRCopyFileToApp("Engine/Core/libs/Audio/libs/OpenAL" "OpenAL64d.dll")
-
-SRCopyFileToApp("lib" "freetype.dll")
-SRCopyFileToApp("lib" "freetyped.dll")
-SRCopyFileToApp("Engine/Core/libs/Graphics" "freetype.dll")
-SRCopyFileToApp("Engine/Core/libs/Graphics" "freetyped.dll")
-
-SRCopyFileToApp("bin" "glew32.dll")
-SRCopyFileToApp("bin" "glew32d.dll")
+#SRCopyFileToApp("Engine/Core/libs/Graphics/EvoVulkan/Core" "EvoVulkan.dll")
+#SRCopyFileToApp("Engine/Core/libs/Graphics/EvoVulkan/Core" "EvoVulkand.dll")
+#
+#SRCopyFileToApp("Engine/Core/libs/Utils/libs/assimp/bin" "assimp-vc142-mtd.dll")
+#SRCopyFileToApp("Engine/Core/libs/Utils/libs/assimp/bin" "assimp-vc142-mtdd.dll")
+#
+#SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc143.md/debug" "PhysXd.dll")
+#SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc143.md/debug" "PhysXCookingd.dll")
+#SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc143.md/debug" "PhysXCommond.dll")
+#SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc143.md/debug" "PhysXFoundationd.dll")
+#
+#SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc142.md/debug" "PhysXd.dll")
+#SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc142.md/debug" "PhysXCookingd.dll")
+#SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc142.md/debug" "PhysXCommond.dll")
+#SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc142.md/debug" "PhysXFoundationd.dll")
+#
+#SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc143.md/release" "PhysX.dll")
+#SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc143.md/release" "PhysXCooking.dll")
+#SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc143.md/release" "PhysXCommon.dll")
+#SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc143.md/release" "PhysXFoundation.dll")
+#
+#SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc142.md/release" "PhysX.dll")
+#SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc142.md/release" "PhysXCooking.dll")
+#SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc142.md/release" "PhysXCommon.dll")
+#SRCopyFileToApp("Engine/Core/libs/Physics/extern/physx/bin/win.x86_64.vc142.md/release" "PhysXFoundation.dll")
+#
+#SRCopyFileToApp("Engine/Core/libs/Utils" "Utils.dll")
+#SRCopyFileToApp("Engine/Core/libs/Utils" "Utilsd.dll")
+#
+#SRCopyFileToApp("Engine/Core/libs/Audio/libs/OpenAL" "OpenAL32.dll")
+#SRCopyFileToApp("Engine/Core/libs/Audio/libs/OpenAL" "OpenAL64.dll")
+#SRCopyFileToApp("Engine/Core/libs/Audio/libs/OpenAL" "OpenAL32d.dll")
+#SRCopyFileToApp("Engine/Core/libs/Audio/libs/OpenAL" "OpenAL64d.dll")
+#
+#SRCopyFileToApp("lib" "freetype.dll")
+#SRCopyFileToApp("lib" "freetyped.dll")
+#SRCopyFileToApp("Engine/Core/libs/Graphics" "freetype.dll")
+#SRCopyFileToApp("Engine/Core/libs/Graphics" "freetyped.dll")
+#
+#SRCopyFileToApp("bin" "glew32.dll")
+#SRCopyFileToApp("bin" "glew32d.dll")
+#
