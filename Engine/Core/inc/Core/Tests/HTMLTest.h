cd41b7e81be691fdc3db10e35aa29e4bf5c1891b
Monika0000 <rotaru5craft@gmail.com>
2024-10-03 20:58:00 +0100

    (HTML) Added litehtml for easy implement web UI.

--- Engine/Core/inc/Core/Tests/HTMLTest.h
+++ Engine/Core/inc/Core/Tests/HTMLTest.h
@@ -14,9 +14,9 @@ namespace SR_CORE_NS::Tests {
     public:
         static bool Run() {
             const SR_UTILS_NS::Path path = "Tests/editor.html";
-            auto&& pPage = SR_UTILS_NS::Web::HTMLParser::Instance().Parse(path);
-            const std::string text = SR_UTILS_NS::Web::HTMLParser::Instance().DebugPageTostring(pPage);
-            SR_DEBUG_LOG("{}", text);
+            //auto&& pPage = SR_UTILS_NS::Web::HTMLParser::Instance().Parse(path);
+            //const std::string text = SR_UTILS_NS::Web::HTMLParser::Instance().DebugPageTostring(pPage);
+            //SR_DEBUG_LOG("{}", text);
             return true;
         }
     };
@@ -25,9 +25,9 @@ namespace SR_CORE_NS::Tests {
     public:
         static bool Run() {
             const SR_UTILS_NS::Path path = "Tests/editor.css";
-            auto&& pCSS = SR_UTILS_NS::Web::CSSParser::Instance().Parse(path);
-            const std::string text = pCSS->ToString();
-            SR_DEBUG_LOG("{}", text);
+            //auto&& pCSS = SR_UTILS_NS::Web::CSSParser::Instance().Parse(path);
+            //const std::string text = pCSS->ToString();
+            //SR_DEBUG_LOG("{}", text);
             return true;
         }
     };
