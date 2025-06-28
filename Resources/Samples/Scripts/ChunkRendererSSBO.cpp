c9e7c54196554be449fe094730687556e6dc00d3
Monika0000 <rotaru5craft@gmail.com>
2025-06-28 20:11:08 +0100

    (ProceduralMesh) Performance tests with ChunkRenderer: SSBO / no SSBO.

--- Resources/Samples/Scripts/ChunkRendererSSBO.cpp
+++ Resources/Samples/Scripts/ChunkRendererSSBO.cpp
@@ -2,6 +2,7 @@
 // Created by Monika on 25.06.2025.
 //
 
+#include <Utils/Input/InputSystem.h>
 #include <Utils/Input/KeyCodes.h>
 #include <Utils/Math/Vector2.h>
 #include <Utils/Math/Noise.h>
@@ -14,7 +15,7 @@
 #include "ChunkRendererHelper.h"
 
 namespace SpaRcle::Scripts::Samples {
-    class ChunkRenderer : public SpaRcle::Scripting::CppBehaviour {
+    class ChunkRendererSSBO : public SpaRcle::Scripting::CppBehaviour {
         SR_CLASS()
     public:
         SR_NODISCARD bool ExecuteInEditMode() const override {
@@ -43,13 +44,13 @@ namespace SpaRcle::Scripts::Samples {
             RegenerateChunkData();
             PreCalcBlockMeshes();
 
-            vertices.resize(totalSides * 4);
-            indices.resize(totalSides * 6);
+            vertices.resize(totalSides * 6);
+            //indices.resize(totalSides * 4);
 
             RebuildVerticesAndIndices();
 
             pMesh->SwapIndexedVertices(vertices);
-            pMesh->SwapIndices(indices);
+            //pMesh->SwapIndices(indices);
         }
 
         void MarkDirty() {
@@ -96,8 +97,7 @@ namespace SpaRcle::Scripts::Samples {
                     return;
                 }
 
-                uint32_t baseIndex = blockInfo.offset * 4;
-                uint32_t indicesIndex = blockInfo.offset * 6;
+                uint32_t baseIndex = blockInfo.offset * 6;
 
                 for (const auto& face : BLOCK_FACES) {
                     if (GetBlockNeighbour(pos.x, pos.y, pos.z, face.side).exists) {
@@ -109,11 +109,10 @@ namespace SpaRcle::Scripts::Samples {
                         pos.y + face.originOffset.y,
                         pos.z + face.originOffset.z,
                         face.normal, face.offsetU, face.offsetV,
-                        baseIndex, indicesIndex
+                        vertices.data() + baseIndex
                     );
 
-                    baseIndex += 4;
-                    indicesIndex += 6;
+                    baseIndex += 6;
                 }
             });
         }
@@ -144,16 +143,30 @@ namespace SpaRcle::Scripts::Samples {
             const SR_MATH_NS::FVector3& normal,
             const SR_MATH_NS::FVector3& offsetU,
             const SR_MATH_NS::FVector3& offsetV,
-            uint32_t baseIndex,
-            uint32_t indicesIndex
+            SR_GRAPH_NS::Vertices::StaticMeshVertexAligned* pVertices
         ) {
             constexpr static SR_MATH_NS::FVector2 uvs[4] = { {0, 0}, {0, 1}, {1, 0}, {1, 1} };
             const SR_MATH_NS::FVector3 origin(static_cast<float_t>(x), static_cast<float_t>(y), static_cast<float_t>(z));
 
-            auto* pVertices = vertices.data() + baseIndex;
-            auto* pIndices = indices.data() + indicesIndex;
+            pVertices[0] = { origin,                     0.f, uvs[0], {}, normal };
+            pVertices[1] = { origin + offsetV,           0.f, uvs[1], {}, normal };
+            pVertices[2] = { origin + offsetU,           0.f, uvs[2], {}, normal };
 
-            pVertices[0] = { origin,                     uvs[0], normal };
+            pVertices[3] = { origin + offsetV,           0.f, uvs[1], {}, normal };
+            pVertices[4] = { origin + offsetU + offsetV, 0.f, uvs[3], {}, normal };
+            pVertices[5] = { origin + offsetU,           0.f, uvs[2], {}, normal };
+
+            /*pVertices[0] = { origin,                     0.f, uvs[0], {}, normal };
+            pVertices[1] = { origin + offsetV,           0.f, uvs[1], {}, normal };
+            pVertices[2] = { origin + offsetU,           0.f, uvs[2], {}, normal };
+            pVertices[3] = { origin + offsetU + offsetV, 0.f, uvs[3], {}, normal };*/
+
+            //pIndices[0] = baseIndex + 0;
+            //pIndices[1] = baseIndex + 1;
+            //pIndices[2] = baseIndex + 2;
+            //pIndices[3] = baseIndex + 3;
+
+            /*pVertices[0] = { origin,                     uvs[0], normal };
             pVertices[1] = { origin + offsetV,           uvs[1], normal };
             pVertices[2] = { origin + offsetU,           uvs[2], normal };
             pVertices[3] = { origin + offsetU + offsetV, uvs[3], normal };
@@ -163,7 +176,7 @@ namespace SpaRcle::Scripts::Samples {
             pIndices[2] = baseIndex + 2;
             pIndices[3] = baseIndex + 1;
             pIndices[4] = baseIndex + 3;
-            pIndices[5] = baseIndex + 2;
+            pIndices[5] = baseIndex + 2;*/
         }
 
         BlockInfo& GetBlock(uint32_t x, uint32_t y, uint32_t z) {
@@ -187,7 +200,7 @@ namespace SpaRcle::Scripts::Samples {
         }
 
     private:
-        SR_HTYPES_NS::FastMemoryArray<SR_GRAPH_NS::Vertices::StaticMeshVertex> vertices;
+        SR_HTYPES_NS::FastMemoryArray<SR_GRAPH_NS::Vertices::StaticMeshVertexAligned> vertices;
         SR_HTYPES_NS::FastMemoryArray<uint32_t> indices;
         bool isDirty = true;
 
@@ -210,4 +223,4 @@ namespace SpaRcle::Scripts::Samples {
     };
 }
 
-#include <Codegen/ChunkRenderer.generated.hpp>
+#include <Codegen/ChunkRendererSSBO.generated.hpp>
