4169a583b70f79fc1fd4c57010d8600afb5a86db
Monika0000 <rotaru5craft@gmail.com>
2025-09-21 13:22:03 +0100

    (Android) Fixed applying icons.

--- Platform/Android/build.bat
+++ Platform/Android/build.bat
@@ -6,5 +6,5 @@ set ARCH=%1
 echo Packing resources...
 call pack_resources.bat assembleRelease
 echo Applying icon...
-call apply_icon.bat.bat 
+call apply_icon.bat 
 call build_no_resources.bat %ARCH%
