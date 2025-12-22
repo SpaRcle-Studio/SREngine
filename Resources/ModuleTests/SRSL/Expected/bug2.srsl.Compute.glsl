808437d2e5bc490b70059adc0733e649b56140eb
Monika0000 <rotaru5craft@gmail.com>
2025-12-22 15:35:06 +0100

    (SRSL) Fixed shaders parsing errors. Working on shadow shader.

---
+++ Resources/ModuleTests/SRSL/Expected/bug2.srsl.Compute.glsl
@@ -0,0 +1,16 @@
+/// [WARNING: THIS FILE WAS CREATED BY SRSL CODE GENERATION]
+
+/// Shader stage: Compute
+/// Shader type: Compute
+
+#version 450
+
+layout (local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+
+layout (set = 0, binding = 0) buffer StorageBuffer_bones {
+	mat4 bones[];
+};
+
+void main() {
+    bones[0];
+}
