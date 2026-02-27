8a2903db59b96f42676df8d86c7c76cd2a0d41f1
Monika0000 <rotaru5craft@gmail.com>
2026-02-27 22:29:14 +0500

    (Render) Working on auto exposure.

---
+++ Resources/ModuleTests/SRSL/Expected/num-test.srsl.Compute.glsl
@@ -0,0 +1,19 @@
+/// [WARNING: THIS FILE WAS CREATED BY SRSL CODE GENERATION]
+
+/// Shader stage: Compute
+/// Shader type: Compute
+
+#version 450
+
+layout (local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
+
+void main() {
+    float a = 1e-6;
+    uint i = 0u;
+    float b = 2.5e3;
+    float c = 1.0e-3;
+    float lum = max(0, 1e-6);
+    int d = 0x10;
+    uint e = 0x20u;
+    uint f = max(0u, 0x30u);
+}
