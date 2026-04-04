fce42187bfc748cbfde9c1825d115b22cf880d50
Monika0000 <rotaru5craft@gmail.com>
2026-04-04 15:47:44 +0500

    (Compression) Fixed wrong compression on first run.

--- CI/clone_samples.bat
+++ CI/clone_samples.bat
@@ -1,4 +1,4 @@
 @echo off
 
 cd ../Resources || exit /b 1
-git clone https://github.com/SpaRcle-Studio/SREngineSampleResources Samples
+git clone https://github.com/SpaRcle-Studio/SREngineSampleResources Samples --depth 1
--- CI/clone_samples.sh
+++ CI/clone_samples.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd ../Resources || exit 1
-git clone https://github.com/SpaRcle-Studio/SREngineSampleResources Samples
+git clone https://github.com/SpaRcle-Studio/SREngineSampleResources Samples --depth 1
